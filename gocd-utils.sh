73389cf33ffbbf2537ce90326fcf8a207d019c15
David Zwarg <david.zwarg@gmail.com>
2020-01-05 23:51:53 -0500

    Update gocd-utils.sh
    Added notes.

--- gocd-utils.sh
+++ gocd-utils.sh
@@ -1,7 +1,12 @@
 #
-#
+# Utility functions to assist the agents in a dockerized GoCD configuration.
 #
 
+#
+# agent_mount is a function that can be called by a dind container to find the 
+# source directory for a volume mount. This is required for any subsequent volume
+# mounts by docker in the tasks.
+#
 function agent_mount () {
   docker inspect `hostname` --format='{{range .Mounts}}{{if eq .Destination "/godata" }}{{.Source}}{{end}}{{end}}'
 }
